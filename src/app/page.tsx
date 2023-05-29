154f504e8e75aeabe57bcaf3fe9d8a034435ca61
Gui Silva <adrianon10@gmail.com>
2023-05-29 19:59:00 -0300

    feat: add components Copyright, Hero, SignIn and EmptyMemories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Link from 'next/link'
-import { User } from 'lucide-react'
-
-import logo from './../assets/logo.svg'
-import Image from 'next/image'
+import { SignIn } from '@/components/SignIn'
+import { Hero } from '@/components/Hero'
+import { Copyright } from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
 
 export default function Home() {
   return (
@@ -10,61 +9,13 @@ export default function Home() {
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16 ">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias
-          </p>
-        </a>
-
-        <div className="space-y-5">
-          <Image src={logo} alt="Time Capsule Logo" loading="lazy" />
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo
-            </p>
-          </div>
-          <Link
-            href="/"
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-          >
-            CADASTRAR LEMBRANÇA
-          </Link>
-        </div>
-
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com 💚 by{' '}
-          <a
-            href="https://github.com/Gui-dev"
-            target="_blank"
-            className="underline hover:text-gray-100"
-            rel="noreferrer"
-          >
-            Gui Silva
-          </a>
-        </div>
+        <SignIn />
+        <Hero />
+        <Copyright />
       </div>
 
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <Link href="/" className="underline hover:text-gray-50">
-              criar agora!
-            </Link>
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export const Copyright = () => {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💚 by{' '}
+      <a
+        href="https://github.com/Gui-dev"
+        target="_blank"
+        className="underline hover:text-gray-100"
+        rel="noreferrer"
+      >
+        Gui Silva
+      </a>
+    </div>
+  )
+}
