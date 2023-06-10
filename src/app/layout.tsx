4151a867905765d9caa0f23d197639f2f5b9a779
Gui Silva <adrianon10@gmail.com>
2023-06-10 02:55:57 -0300

    feat: add memory list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const isAuthenticated = cookies().has('token')
-
   return (
     <html lang="en">
       <body
@@ -41,7 +40,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <Copyright />
           </div>
 
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
